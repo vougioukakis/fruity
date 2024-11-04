f1a5ad7b7c3f22a2f70c54b7b130cefea2c82d49
Nikos Vougioukakis <vougioukakis99@gmail.com>
2024-11-05 00:56:46 +0200

    set run  button outside form

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 A web app with a simple fruit image recognition model that provides nutritional information about the recognized fruit, as well as random fruit facts!
 The application calls my own API to retrieve the information and the facts. The API also supports adding
 new fruit facts from the website.
+
+The server is made using Quart with Python, and the API using Express with JavaScript.
--- fruit-img-rec/static/img-rec-app.js
+++ fruit-img-rec/static/img-rec-app.js
@@ -20,20 +20,9 @@ async function getFact() {
 		`;
 	}
 }
-/**
-Adds an event listener to the diagnose button to handle form submission.
-Inside the event listener, it fetched the results and populates the results list,
-resets the form,and marks the last diagnosis as done.
-This function basically controls the main loop of uploading and diagnosing.
 
-About the backend response:
-JSON form with disease_1, disease_2 and disease_3 fields/
-If something goes wrong or one of them is blank, make sure it has an error field.
-@returns {void}
- */
-async function addEvListenerToDiagnoseButton() {
-
-	// TODO: add event listener to 'submit' form and handle it here instead of calling /upload from the html
+async function addEventListenerDiagnoseButton(){
+	// submit event listener for the form to call uploadImage once submitted
 	const form = document.querySelector("form.file-upload");
 	console.log("trying to add event listener...");
 
@@ -101,14 +90,12 @@ function showErrorResult(string) {
 /**
  adds an event listener to the image input field to handle file selection.
  When a file is chosen, the function updates the file name label,
- and displays
- the selected image in the output-area.
+ and displays the selected image in the output-area.
 @returns {void}
  */
 function addEvListenerToImgInputField() {
-	console.log("adding event listener to image input field...");
 	const imageInputField = document.getElementById("image-input");
-	console.log('this has been updated');
+
 	imageInputField.addEventListener("change", function () {
 		try {
 			console.log("showing image...");
@@ -145,12 +132,11 @@ function checkToDisableDiagnoseButton() {
 /**
 runs when DOM is loaded.
 initializes the application by adding event listeners to relevant elements and calling necessary functions.
-This function should be called every time a chat is switched to ensure proper functionality.
 @returns {void}
  */
 function init() {
 	console.log("init running");
-	addEvListenerToDiagnoseButton();
+	addEventListenerDiagnoseButton();
 	addEvListenerToImgInputField();
 	addEvListenerToGetFactBtn();
 }
